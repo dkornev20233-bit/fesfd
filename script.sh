05a33c67f8819379f11a62f18ee6c6ef409ba847
vervinsky <dkornev20233@gmail.com>
2026-04-30 20:10:55 +0300

    2 fix

--- script.sh
+++ script.sh
@@ -2,12 +2,17 @@
 
 ZSHRC="$HOME/.zshrc"
 
-grep -q 'DebianHuinya' "$ZSHRC" && exit 0
-
+if ! grep -q "DebianHuinya" "$ZSHRC"; then
 cat >> "$ZSHRC" << 'EOF'
 
 # custom prompt
 PROMPT="DebianHuinya %~ ~> "
 EOF
+fi
 
-exec zsh -i
+# применяем сразу если ты в zsh
+if [ -n "$ZSH_VERSION" ]; then
+  source "$ZSHRC"
+else
+  exec zsh
+fi
